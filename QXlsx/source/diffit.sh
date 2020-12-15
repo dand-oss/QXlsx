ec0e5a85cfe5846084820d7440e68722b764c49b
Dan Dees <dand@appsmiths.com>
2020-12-15 16:26:55 +0700

    diff tools

---
+++ QXlsx/source/diffit.sh
@@ -0,0 +1,41 @@
+declare -ar filez=(
+'xlsxabstractooxmlfile.cpp'
+'xlsxabstractsheet.cpp'
+'xlsxcell.cpp'
+'xlsxcellformula.cpp'
+'xlsxcelllocation.cpp'
+'xlsxcellrange.cpp'
+'xlsxcellreference.cpp'
+'xlsxchart.cpp'
+'xlsxchartsheet.cpp'
+'xlsxcolor.cpp'
+'xlsxconditionalformatting.cpp'
+'xlsxcontenttypes.cpp'
+'xlsxdatavalidation.cpp'
+'xlsxdatetype.cpp'
+'xlsxdocpropsapp.cpp'
+'xlsxdocpropscore.cpp'
+'xlsxdocument.cpp'
+'xlsxdrawinganchor.cpp'
+'xlsxdrawing.cpp'
+'xlsxformat.cpp'
+'xlsxmediafile.cpp'
+'xlsxnumformatparser.cpp'
+'xlsxrelationships.cpp'
+'xlsxrichstring.cpp'
+'xlsxsharedstrings.cpp'
+'xlsxsimpleooxmlfile.cpp'
+'xlsxstyles.cpp'
+'xlsxtheme.cpp'
+'xlsxutility.cpp'
+'xlsxworkbook.cpp'
+'xlsxworksheet.cpp'
+'xlsxzipreader.cpp'
+'xlsxzipwriter.cpp'
+)
+
+for f in ${filez[@]} ; do
+    echo ""
+    echo "$f"
+    diff "$f" /i/ports/repo/qxlsx/src/xlsx || true
+done
